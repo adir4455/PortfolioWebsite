89d0031f25436ddcaf926d2a543a306d5c568d37
adir4455 <adir4455@gmail.com>
2021-10-30 20:58:31 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -10,49 +10,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="text-fields">
-          <input
-            type="text"
-            className="text-input name-input"
-            placeholder="Name"
-            name="name"
-          />
-          <input
-            type="text"
-            className="text-input subject-input"
-            placeholder="Subject"
-            name="subject"
-          />
-          <input
-            type="email"
-            className="text-input email-input"
-            placeholder="Email Address"
-            name="email"
-          />
-          <input
-            type="text"
-            className="text-input phone-input"
-            placeholder="Phone Number"
-            name="phone"
-          />
-          <textarea
-            type="text"
-            className="text-input message-input"
-            placeholder="Enter Message"
-            name="message"
-          ></textarea>
-        </div>
-        <button type="submit" className="btn-dark">
-          Submit
-        </button>
-      </form>
       <Router>
         <Switch>
           <Route exact path="/">

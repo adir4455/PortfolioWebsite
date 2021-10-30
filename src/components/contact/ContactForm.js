44ddb46d6cc813981ce61717aa406bde30cb8ef4
adir4455 <adir4455@gmail.com>
2021-10-30 21:42:16 +0300

    form fix

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,20 +1,35 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 function ContactForm() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_ahq9w76",
+        e.target,
+        "user_qkiMCZkzMTkaAfEJdyzq1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
   return (
     <section id="contact-a" className="text-center py-3">
       <div className="container">
         <h2 className="section-title">Contact Me</h2>
         <div className="bottom-line"></div>
         <p className="lead">Here is how you can reach me</p>
-        <form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit="submit"
-        >
-          <input type="hidden" name="form-name" value="contact" />
+        <form onSubmit={sendEmail}>
           <div className="text-fields">
             <input
               type="text"

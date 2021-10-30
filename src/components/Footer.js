61c959cba76cb57347ae7083325ed22a720fc0b6
adir4455 <adir4455@gmail.com>
2021-10-30 20:17:00 +0300

    bug fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,10 +13,10 @@ function Footer() {
       <div className="footer-content container">
         <p>Copyright &copy; 2021, All Rights Reserved</p>
         <div className="social">
-          <a href="https://www.linkedin.com/in/adir-elmakyes/" target="_blank">
+          <a href="https://github.com/adir4455" target="_blank">
             <FontAwesomeIcon icon={faGithub} className="fab fa-github" />
           </a>
-          <a href="https://github.com/adir4455" target="_blank">
+          <a href="https://www.linkedin.com/in/adir-elmakyes/" target="_blank">
             <FontAwesomeIcon icon={faLinkedinIn} className="fab fa-linkedin" />
           </a>
         </div>

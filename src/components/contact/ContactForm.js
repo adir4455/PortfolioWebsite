3e54d85df6c9ef63b8d8b7e374a8232b533f304a
adir4455 <adir4455@gmail.com>
2021-10-31 16:32:46 +0200

    submit update

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,7 +1,10 @@
 import React from "react";
 import emailjs from "emailjs-com";
+import { useGlobalContext } from "../../context";
 
 function ContactForm() {
+  const { isSubmitted, setIsSubmitted } = useGlobalContext();
+
   function sendEmail(e) {
     e.preventDefault();
 
@@ -26,50 +29,57 @@ function ContactForm() {
   return (
     <section id="contact-a" className="text-center py-3">
       <div className="container">
-        <h2 className="section-title">Contact Me</h2>
-        <div className="bottom-line"></div>
-        <p className="lead">Here is how you can reach me</p>
-        <form onSubmit={sendEmail}>
-          <div className="text-fields">
-            <input
-              type="text"
-              className="text-input name-input"
-              placeholder="Name"
-              name="name"
-              required
-            />
-            <input
-              type="text"
-              className="text-input subject-input"
-              placeholder="Subject"
-              name="subject"
-              required
-            />
-            <input
-              type="email"
-              className="text-input email-input"
-              placeholder="Email Address"
-              name="email"
-              required
-            />
-            <input
-              type="text"
-              className="text-input phone-input"
-              placeholder="Phone Number"
-              name="phone"
-            />
-            <textarea
-              type="text"
-              className="text-input message-input"
-              placeholder="Enter Message"
-              name="message"
-              required
-            ></textarea>
+        {!isSubmitted && <h2 className="section-title">Contact Me</h2>}
+        {!isSubmitted && <div className="bottom-line"></div>}
+        {!isSubmitted && <p className="lead">Here is how you can reach me</p>}
+        {!isSubmitted && (
+          <form onSubmit={sendEmail}>
+            <div className="text-fields">
+              <input
+                type="text"
+                className="text-input name-input"
+                placeholder="Name"
+                name="name"
+              />
+              <input
+                type="text"
+                className="text-input subject-input"
+                placeholder="Subject"
+                name="subject"
+              />
+              <input
+                type="email"
+                className="text-input email-input"
+                placeholder="Email Address"
+                name="email"
+              />
+              <input
+                type="text"
+                className="text-input phone-input"
+                placeholder="Phone Number"
+                name="phone"
+              />
+              <textarea
+                type="text"
+                className="text-input message-input"
+                placeholder="Enter Message"
+                name="message"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="btn-dark"
+              onClick={() => setIsSubmitted(true)}
+            >
+              Submit
+            </button>
+          </form>
+        )}
+        {isSubmitted && (
+          <div className="submit">
+            <p>Successfully Submitted!</p>
           </div>
-          <button type="submit" className="btn-dark">
-            Submit
-          </button>
-        </form>
+        )}
       </div>
     </section>
   );
--- src/context.js
+++ src/context.js
@@ -4,7 +4,13 @@ import React, { useState, useContext, useEffect } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  return (
+    <AppContext.Provider value={{ isSubmitted, setIsSubmitted }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
